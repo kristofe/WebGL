eeddcdd3a93f7017984cec233b3817b5006d2582
Kristofer Schlachter <kristofe72@gmail.com>
2013-12-18 16:21:00 -0500

    Tested on nvidia

--- light.js
+++ light.js
@@ -5,6 +5,6 @@ function Light(gl){
   this.transform = new Transform();
   this.projection = new Matrix44();
   
-  this.projection.perspective(90 * Math.PI/180.0, 1.0, 0.1, 20.0);
+  this.projection.perspective(45 * Math.PI/180.0, 1.0, 0.1, 20.0);
 }
 
