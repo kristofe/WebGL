618414df6936fb88e2f6a52b5a9d8becb794bae5
Kristofer Schlachter <kristofe72@gmail.com>
2014-01-24 13:29:27 -0500

    added new material for visualizing sim

--- wavesim.js
+++ wavesim.js
@@ -6,6 +6,7 @@ WaveSim.prototype.constructor = WaveSim;
 
 function WaveSim(gl){
   Model.call(this, gl);
+  this.simulationMaterial = new Material(this.gl);
 
   this.mesh.createScreenQuad(new Vector2(-1,-1), new Vector2(1,1));
   this.rt0 = new RenderTarget(gl, 512,512, gl.FLOAT);
@@ -16,6 +17,7 @@ function WaveSim(gl){
   this.rt0.viewportSync(512,512);
   this.rt1.viewportSync(512,512);
   this.setupMaterial(this.gl);
+  this.setupSimulationMaterial(this.gl);
 }
 
 WaveSim.prototype.drawRenderer = function(renderer){
@@ -23,19 +25,19 @@ WaveSim.prototype.drawRenderer = function(renderer){
   this.gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   this.rt0.bind();
-  this.drawQuad();
+  this.drawQuad(this.simulationMaterial);
   this.rt0.unbind();
 
-  this.drawQuad();
+  this.drawQuad(this.material);
   this.debugDraw(renderer);
 
   this.swapRenderTargets();
 };
 
-WaveSim.prototype.drawQuad = function() {
-  this.material.bind(this.mesh);
-  this.material.setModelUniforms(this);
-  this.material.setRendererUniforms(renderer);
+WaveSim.prototype.drawQuad = function(material) {
+  material.bind(this.mesh);
+  material.setModelUniforms(this);
+  material.setRendererUniforms(renderer);
   this.gl.drawArrays(
                       this.mesh.primitiveType,
                       0, 
@@ -47,8 +49,9 @@ WaveSim.prototype.swapRenderTargets = function() {
   var tmp = this.rt1;
   this.rt1 = this.rt0;
   this.rt0  = tmp;
-  this.material.setTextureWithSlot(this.rt0.texture, 0);
-  this.material.setTextureWithSlot(this.rt1.texture, 1);
+  this.simulationMaterial.setTextureWithSlot(this.rt0.texture, 0);
+  this.simulationMaterial.setTextureWithSlot(this.rt1.texture, 1);
+  this.material.setTexture(this.rt0.texture);
 };
 
 WaveSim.prototype.debugDraw = function(renderer){
@@ -58,6 +61,47 @@ WaveSim.prototype.debugDraw = function(renderer){
 
 
 WaveSim.prototype.setupMaterial = function(gl){
+  var fsSource= " precision mediump float;\n\
+    uniform sampler2D uTexture01;\n\
+    varying vec2 vUV;\n\
+\n\
+    void main(void) {\n\
+      float c =  texture2D(uTexture01,vUV).x * 0.5 + 0.5;\n\
+      gl_FragColor =  vec4(c,c,c,1);\n\
+    } ";
+
+  var vsSource= " precision mediump float;\n\
+    attribute vec3 aPosition;\n\
+    attribute vec3 aNormal;\n\
+    attribute vec2 aUV;\n\
+    attribute vec4 aColor;\n\
+\n\
+    uniform sampler2D uTexture01;\n\
+\n\
+    varying vec2 vUV;\n\
+    varying vec4 vVertColor;\n\
+\n\
+\n\
+    void main(void) {\n\
+\n\
+        gl_Position = vec4(aPosition, 1.0);\n\
+        vVertColor = aColor;\n\
+        \n\
+        vUV = aUV.xy;\n\
+    }\n\
+   ";
+
+  this.material.shader.initShaderWithSource(fsSource,vsSource);
+  this.material.zTest = false;
+  this.material.zWrite = false;
+  this.material.lineWidth = 1.0;
+  this.material.setTexture(this.rt0.texture);
+  this.material.addTexture(this.rt1.texture);
+
+};
+
+
+WaveSim.prototype.setupSimulationMaterial = function(gl){
   var fsSource= " precision mediump float;\n\
     uniform vec2 uViewportSize;\n\
     uniform vec2 uMouseLocation;\n\
@@ -124,11 +168,11 @@ WaveSim.prototype.setupMaterial = function(gl){
     }\n\
    ";
 
-  this.material.shader.initShaderWithSource(fsSource,vsSource);
-  this.material.zTest = false;
-  this.material.zWrite = false;
-  this.material.lineWidth = 1.0;
-  this.material.setTexture(this.rt0.texture);
-  this.material.addTexture(this.rt1.texture);
+  this.simulationMaterial.shader.initShaderWithSource(fsSource,vsSource);
+  this.simulationMaterial.zTest = false;
+  this.simulationMaterial.zWrite = false;
+  this.simulationMaterial.lineWidth = 1.0;
+  this.simulationMaterial.setTexture(this.rt0.texture);
+  this.simulationMaterial.addTexture(this.rt1.texture);
 
 };
